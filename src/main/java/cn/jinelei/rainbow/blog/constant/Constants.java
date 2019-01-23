6ba903f1718879e84d55ce9088d268bd595bb2a3
jinelei <jinelei@163.com>
2019-01-23 15:13:26 +0800

    添加常量

--- src/main/java/cn/jinelei/rainbow/blog/constant/Constants.java
+++ src/main/java/cn/jinelei/rainbow/blog/constant/Constants.java
@@ -4,7 +4,32 @@ package cn.jinelei.rainbow.blog.constant;
  * @author zhenlei
  */
 public class Constants {
+    public static final Integer INVAILD_VALUE = -1;
+    public static final String DEFAULT_STRING = "";
     public static final String AUTHORIZATION = "authorization";
     public static final String CURRENT_USER_ID = "current_user_id";
-    public static final String REQUIRED_USER= "required_user";
+    public static final String REQUIRED_USER = "required_user";
+    public static final String PAGE = "page";
+    public static final String SIZE = "size";
+    public static final String TAGS = "tags";
+    public static final String CATEGORY = "category";
+    public static final String AUTHOR = "author";
+    public static final String TITLE = "title";
+    public static final String USERNAME = "username";
+    public static final String NICKNAME = "nickname";
+    public static final String PHONE = "phone";
+    public static final String CITY = "city";
+    public static final String PROVINCE = "province";
+    public static final String EMAIL = "email";
+    public static final String DESC_FILTERS = "descBy";
+    public static final String ASC_FILTERS = "ascBy";
+    public static final String COMMA_SPLIT = ",";
+    public static final String NAME = "name";
+    public static final String SUMMARY = "summary";
+    public static final String CONTENT = "content";
+    public static final String USER_ID = "user_id";
+    public static final String ARTICLE_ID = "article_id";
+    public static final String ARTICLE = "article";
+    public static final String CATEGORY_CREATOR = "categoryCreator";
+    public static final String COMMENTATOR= "commentator";
 }
