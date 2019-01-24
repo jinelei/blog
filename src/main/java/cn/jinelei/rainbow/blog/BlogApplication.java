d259fe62ea8e3b3e62e62784422ac4d0c4b06bad
jinelei <jinelei@163.com>
2019-01-24 15:29:42 +0800

    添加跨域的支持

--- src/main/java/cn/jinelei/rainbow/blog/BlogApplication.java
+++ src/main/java/cn/jinelei/rainbow/blog/BlogApplication.java
@@ -6,11 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -24,17 +30,10 @@ public class BlogApplication extends WebMvcConfigurerAdapter {
     }
 
     @Autowired
-    AuthorizationInterceptor authorizationInterceptor;
-
-    @Autowired
     CurrentUserArgumentResolver currentUserArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 多个拦截器组成一个拦截器链
-        // addPathPatterns 用于添加拦截规则
-        // excludePathPatterns 用于排除拦截
-//        registry.addInterceptor(authorizationInterceptor).addPathPatterns("/**");
         registry.addInterceptor(currentUserArgumentResolver).addPathPatterns("/**");
         super.addInterceptors(registry);
     }
@@ -47,7 +46,15 @@ public class BlogApplication extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .exposedHeaders("Authorization");
+        super.addCorsMappings(registry);
     }
+
 }
 
